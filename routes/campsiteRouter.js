4837985cd42fe82704ab3f07d3ceabab805eb7c7
Felj Abas <feljstax@gmail.com>
2022-12-19 03:53:44 -0800

    Week 2 Workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -18,9 +18,9 @@ campsiteRouter.route('/')
         //Create Campsite document and send to MongoDB server
         Campsite.create(req.body)
             .then(campsite => {
-                console.log('Campsite Created ', campsite)
+                console.log('Campsite Created ', campsite);
                 res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json')
+                res.setHeader('Content-Type', 'application/json');
                 res.json(campsite);
             })
             .catch(err => next(err));
@@ -43,9 +43,9 @@ campsiteRouter.route('/:campsiteId')
     .get((req, res, next) => {
         Campsite.findById(req.params.campsiteId)
             .then(campsite => {
-                res.status = 200;
-                res.setHeader('Content-Type', 'application/json')
-                res.json(campsite)
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(campsite);
             })
             .catch(err => next(err))
     })
@@ -57,7 +57,7 @@ campsiteRouter.route('/:campsiteId')
             $set: req.body
         }, { new: true })
             .then(campsite => {
-                res.status = 200;
+                res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json')
                 res.json(campsite)
             })
@@ -66,7 +66,7 @@ campsiteRouter.route('/:campsiteId')
     .delete((req, res, next) => {
         Campsite.findByIdAndDelete(req.params.campsiteId)
             .then(response => {
-                res.status = 200;
+                res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json')
                 res.json(response)
             })
