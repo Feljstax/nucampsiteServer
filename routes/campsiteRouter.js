6f781392d5e2bbd148c4802487282eb738c1b74b
Felj Abas <feljstax@gmail.com>
2022-12-19 04:09:27 -0800

    Slight refactor Workshop 2

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -68,7 +68,7 @@ campsiteRouter.route('/:campsiteId')
             .then(response => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json')
-                res.json(response)
+                res.json(response);
             })
             .catch(err => next(err));
     });
